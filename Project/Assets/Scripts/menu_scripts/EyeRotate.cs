c88afda9324805645ce288a55729879f17668683
wjsghtjf <37579664+wjsghtjf@users.noreply.github.com>
2020-08-31 00:08:01 +0900

    [ADD] Eye rotate -hoseol

---
+++ Project/Assets/Scripts/menu_scripts/EyeRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EyeRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float turnspeed = 40f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f,turnspeed * Time.deltaTime);
+    }
+}
