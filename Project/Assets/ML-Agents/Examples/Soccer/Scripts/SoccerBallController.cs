5ab7b8d8dcaf23ec57b0fe5f9d572b7bf9710126
wjsghtjf <37579664+wjsghtjf@users.noreply.github.com>
2020-09-07 01:48:41 +0900

    [ADD] soccer play

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController.cs
@@ -7,27 +7,78 @@ public class SoccerBallController : MonoBehaviour
     public string purpleGoalTag; //will be used to check if collided with purple goal
     public string blueGoalTag; //will be used to check if collided with blue goal
     public GameObject finishUI;
+    public GameObject before;
+    public GameObject after;
     public Text winner;
+    public Text B1;
+    public Text R1;
+    public Text B2;
+    public Text R2;
     bool check = false;
+   
     void OnCollisionEnter(Collision col)
     {
         if (!check)
         {
-            if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
-            {
-                check = true;
-                finishUI.SetActive(true);
-                area.GoalTouched(AgentSoccer.Team.Blue);
-                winner.text = "Red";
-    
-            }
-            if (col.gameObject.CompareTag(blueGoalTag)) //ball touched blue goal
-            {
-                check = true;
-                finishUI.SetActive(true);
-                area.GoalTouched(AgentSoccer.Team.Purple);
-                winner.text = "Blue";
-            }
+           
+                if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
+                {
+                    int score = int.Parse(B1.text);
+                    score++;
+                    B1.text = score.ToString();
+                    B2.text = B1.text;
+                    check = true;
+                    area.GoalTouched(AgentSoccer.Team.Blue);
+                    int Bs = int.Parse(B1.text);
+                    int Rs = int.Parse(R1.text);
+                    if (Rs == 2)
+                    {
+                        finishUI.SetActive(true);
+                        winner.text = "Red";
+                    }
+                    else if (Bs == 2)
+                    {
+                       finishUI.SetActive(true);
+                        winner.text = "Blue";
+                    }
+                    else
+                    {
+                        after.SetActive(true);
+                        before.SetActive(false);
+                    }
+
+                }
+                if (col.gameObject.CompareTag(blueGoalTag)) //ball touched blue goal
+                {
+                    int score = int.Parse(R1.text);
+                    score++;
+                    R1.text = score.ToString();
+                    R2.text = R1.text;
+                    check = true;
+                    area.GoalTouched(AgentSoccer.Team.Purple);
+                    int Bs = int.Parse(B1.text);
+                    int Rs = int.Parse(R1.text);
+                    if (Rs == 2)
+                    {
+                        
+                        finishUI.SetActive(true);
+                        winner.text = "Red";
+                    }
+                    else if (Bs == 2)
+                    {
+                        
+                        finishUI.SetActive(true);
+                        winner.text = "Blue";
+                    }
+                    else
+                    {
+                        after.SetActive(true);
+                        before.SetActive(false);
+                    }
+
+                }
+
+            
         }
     }
 }
